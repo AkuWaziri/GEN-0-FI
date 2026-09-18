5e2c98d8faa48280c437941c123bfe3785df1311
Aku Waziri <32820229+AkuWaziri@users.noreply.github.com>
2026-09-18 15:37:57 +0100

    Fix Vercel ESM serverless import resolution

--- api/blockchain/arc/summary/[address].ts
+++ api/blockchain/arc/summary/[address].ts
@@ -1,5 +1,5 @@
 import { isAddress } from 'viem';
-import { arcClient, fetchCompleteWalletState } from '../../../../src/services/blockchain/arcService';
+import { arcClient, fetchCompleteWalletState } from '../../../../src/services/blockchain/arcService.js';
 
 export default async function handler(req: any, res: any) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
